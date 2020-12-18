90461bb2ba2c330d0dae164d743a09d2eea27749
yurir <yuri.ritvin@pipl.com>
2020-12-18 16:42:48 +0200

    Adding new Strategy

--- app/apps.py
+++ app/apps.py
@@ -12,12 +12,20 @@ class AppConfig(AppConfig):
             # importing model classes
             from .runner import Runner  # or...
             from .models import Strategy  # or...
-            s, _ = Strategy.objects.get_or_create(name="strategy1", python_model='app.strategies.amit_strategy')
+            s, _ = Strategy.objects.get_or_create(python_model='app.strategies.amit_strategy')
+            s.name = "Original Strategy"
             s.start_at = now()
             s.save()
 
-            s, _ = Strategy.objects.get_or_create(name="strategy2", python_model='app.strategies.corona_watcher')
+            s, _ = Strategy.objects.get_or_create(python_model='app.strategies.corona_watcher')
+            s.name = "Pick up stock that didn't recovered from corona"
             s.start_at = now()
+            s.save()
+
+            s, _ = Strategy.objects.get_or_create(python_model='app.strategies.fast_strategy')
+            s.start_at = now()
+            s.name = "Fast buy and sell"
+            s.description = "Buy after 2 times increase, check every 1 minute, sell after 0.5%, stock poll is all stock increased from the last 5 days"
             s.save()
             # if os.environ.get('RUN_MAIN', None):
             #     Runner.runner(force=False)
---
+++ app/strategies/fast_strategy.py
@@ -0,0 +1,64 @@
+import logging
+
+from stocks import utils
+from stocks.algorithm import AlgorithmBase
+from stocks.background import BackgroundRunner
+from stocks.constants import TOP_1000_STOCKS
+from stocks.utils import run_async
+
+logger = logging.getLogger(__name__)
+
+
+class MyStrategy(AlgorithmBase, BackgroundRunner):
+
+    def __init__(self, sell_threshold=2, interval=15, percentage_decrease=10, number_of_increase_in_row=4,
+                 run_on_business_days=False,
+                 *args,
+                 **kwargs):
+        super(MyStrategy, self).__init__(*args, **kwargs)
+
+        self.sleep_time = 1
+        self.sell_threshold = sell_threshold
+        self.interval = interval
+
+        self.sell_threshold = 0.5
+
+        self.minutes_between_increase_in_row = 1
+        self.number_of_increase_in_row = 2
+
+        # We are checking for stock that decreased in 11%
+        self.percentage_decrease = percentage_decrease
+        self.STOCK_LIST = TOP_1000_STOCKS
+
+        self.iteration = 0
+
+        self.run_on_business_days = run_on_business_days
+
+        self.last_time = None
+        self.init()
+
+    def init(self):
+        pass
+
+    @run_async
+    def check_stock_in_criteria(self, stock):
+        try:
+            hist = stock.get_price_at(period="5d", auto_adjust=False).Close[0]
+            current_price = stock.price
+            price_change = current_price - hist
+            percentage_change = utils.get_change(hist, current_price)
+            """
+                Checking if the stock has a decrese momentom in the last month.
+                checking if the stock has decrease more than %self.percentage_decrease
+            """
+            if price_change < 0 and percentage_change > self.percentage_decrease:
+                logger.info(
+                    "Stock %s entered to the watch list, Price at before corona peak ago %s, Price now %s change: %s",
+                    stock.name,
+                    hist,
+                    current_price,
+                    percentage_change)
+
+                self.stock_to_watch.add(stock)
+        except Exception as e:
+            logger.warn("Error getting data about %s: %s", stock.name, e)
